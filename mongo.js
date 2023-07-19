7c3e6199f0d0e8a08d4e049307117397dc360661
PetteriV99 <petterivanttinen99@gmail.com>
2023-07-19 15:32:07 +0300

    Task 3.12

--- mongo.js
+++ mongo.js
@@ -13,12 +13,12 @@ const url =
 mongoose.set('strictQuery', false)
 mongoose.connect(url)
 
-const noteSchema = new mongoose.Schema({
+const personSchema = new mongoose.Schema({
   name: String,
   number: String,
 })
 
-const Person = mongoose.model('Person', noteSchema)
+const Person = mongoose.model('Person', personSchema)
 
 // Extra arguments are not taken into account
 
