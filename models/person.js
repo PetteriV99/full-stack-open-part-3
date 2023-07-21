a18b3a7af744d476b3f885a422785b99302b940d
PetteriV99 <petterivanttinen99@gmail.com>
2023-07-21 15:10:30 +0300

    Complete task 3.20

--- models/person.js
+++ models/person.js
@@ -19,8 +19,14 @@ const personSchema = new mongoose.Schema({
     name: String,
     number: {
         type: String,
-        minlength: 3,
-        required: true
+        minlength: 8,
+        validate: {
+            validator: function (v) {
+                return /^(?:\d{2,3}-\d+)$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
     },
 })
 
